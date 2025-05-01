dc8e7a81bd15dbb28c6793061dccf9d007c9370a
ali reza azdast <azdast47@gmail.com>
2025-05-01 17:03:46 +0330

    well i did little bit of the work but i have to go so let continue tommorow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Header from "./components/Header"
 
 
 function App() {
 
 
   return (
-   <><h1 className="text-4xl text-orange-50">hellow</h1></>
+   <>
+    <Header/>
+   </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='w-full bg-secondary text-white p-1'>
+            <p className='text-center font-bold'>با عضویت در فیت لند اولین ارسال را مهمون ما باش :)</p>
+        </div>
+        <div className='my-container mt-10'>
+            <div className='felx items-center justify-between'>
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
